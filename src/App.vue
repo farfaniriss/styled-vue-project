a4016f37b6213a7daef0d65ea96d8d21ebb8e9dc
Iris Sofia Farfan Castañeda <isfc@outlook.es>
2019-04-08 17:53:00 -0500

    added styles

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav">
+      <img
+        class="logo-left"
+        src="https://nd95x20v95u1skvw616h3puj-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/logo.png"
+      >
+      <ul class="nav-tabs">
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
+      <div class="link-toggle">
+        <i class="fas fa-bars"></i>
+      </div>
     </div>
-    <router-view/>
+    <div class="content">
+      <transition name="component-fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss" src="./assets/styles.scss" >
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="main-container">
+    <h1>Container</h1>
+    <div class="main-component">
+      <div class="container-row">
+        Label 1
+        <input type="text" v-model="label1" placeholder="label 1">
+      </div>
+      <div class="container-row">
+        Label 2
+        <input type="text" v-model="label2" placeholder="label 2">
+      </div>
+      <div class="container-row">
+        Label 3
+        <input type="text" v-model="label3" placeholder="label 3">
+      </div>
+      <div class="container-row">
+        <button type="button" class="btn-primary">Primary</button>
+        <button type="button" class="btn-secondary">Secondary</button>
+        <button type="button" class="btn-danger">Danger</button>
+      </div>
+      <div class="container-row">
+        <button type="button" class="btn-outline-primary">Outline primary</button>
+        <button type="button" class="btn-outline-danger">Outline danger</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  label1 = "";
+  label2 = "";
+  label3 = "";
+}
 </script>
